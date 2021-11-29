763f1f5aa5b6cfed9dda71fcb70278976e96fcdc
Oladeji Daniel <oladejidaniel456@gmail.com>
2021-11-29 07:09:24 -0800

    Finish and debug the project

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
 $(document).ready(function(){
     //introduction to javascript
     $("#intro-click").click(function(){
-        $("#js-click").hide();
-        $("#js-intro").show()
+        $("#js-click").slideUp(1000).delay(1000);
+        $("#js-intro").slideDown(1000)
 
        } )
     $("#intro-hide").click(function(){
-        $("#js-intro").hide();
-        $("#js-click").show();
+        $("#js-intro").slideUp(1000);
+        $("#js-click").slideDown(1000);
     });
     //Functions
     $("#func-show").click(function(){
-        $("#function").hide();
-        $("#function-def").show();
+        $("#function").hide(1000);
+        $("#function-def").show(1000);
     })
     $("#func-hide").click(function(){
-        $("#function-def").hide();
-        $("#function").show();
+        $("#function-def").hide(1000);
+        $("#function").show(1000);
     })
     //Variables
     $("#var-show").click(function(){
-        $("#variable").hide();
-        $("#var-sec").show();
+        $("#variable").hide(300);
+        $("#var-sec").show(300);
     })
     $("#var-hide").click(function(){
-        $("#var-sec").hide();
-        $("#variable").show();
+        $("#var-sec").hide(300);
+        $("#variable").show(300);
     })
     //Methods
     $("#meth-show").click(function(){
-        $("#method").hide();
-        $("#meth-sec").show();
+        $("#method").hide(300);
+        $("#meth-sec").show(300);
     })
     $("#meth-hide").click(function(){
-        $("#meth-sec").hide();
-        $("#method").show();
+        $("#meth-sec").hide(300);
+        $("#method").show(300);
     })
 })
